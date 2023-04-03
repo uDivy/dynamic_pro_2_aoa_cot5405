c0719d285b040f55879f084056eb9e81af5e9998
Raj Vora <rajvora1998@gmail.com>
2023-04-03 14:45:56 -0400

    modify task 1,2,3,4 to std input

--- task3.py
+++ task3.py
@@ -22,35 +22,35 @@ def maximal_square(m, n, h, matrix):
                 
     return boundary
 
-# # Read the first line of input and split it into three variables
-# m, n, h = map(int, input().split())
+# Read the first line of input and split it into three variables
+m, n, h = map(int, input().split())
 
-# # Initialize an empty list to store the matrix
-# matrix = []
+# Initialize an empty list to store the matrix
+matrix = []
 
-# # Iterate over the remaining lines of input and append each row to the matrix
-# for _ in range(m):
-#     row = list(map(int, input().split()))
-#     matrix.append(row)
+# Iterate over the remaining lines of input and append each row to the matrix
+for _ in range(m):
+    row = list(map(int, input().split()))
+    matrix.append(row)
 
 # Comparative Study ##########################################
 # open the file for reading
-with open(str(sys.argv[1]), 'r') as f:
+# with open(str(sys.argv[1]), 'r') as f:
     
-    # read the first line and extract the dimensions
-    dimensions = f.readline().split()
-    m = int(dimensions[0])
-    n = int(dimensions[1])
-    h = int(dimensions[2])
+#     # read the first line and extract the dimensions
+#     dimensions = f.readline().split()
+#     m = int(dimensions[0])
+#     n = int(dimensions[1])
+#     h = int(dimensions[2])
     
-   # initialize the matrix variable
-    matrix = [[0]*n for i in range(m)]
+#    # initialize the matrix variable
+#     matrix = [[0]*n for i in range(m)]
     
-    # read the remaining lines and populate the matrix variable
-    for i in range(m):
-        row = f.readline().split()
-        for j in range(n):
-            matrix[i][j] = int(row[j])
+#     # read the remaining lines and populate the matrix variable
+#     for i in range(m):
+#         row = f.readline().split()
+#         for j in range(n):
+#             matrix[i][j] = int(row[j])
 
 ############################################################
 
@@ -61,5 +61,5 @@ end_time = time.time()  # record the end time
 for val in boundary:
     print(val+1,end=' ')
 runtime = end_time - start_time  # calculate the runtime
-print()
-print(f"Runtime: {runtime:.4f} seconds")
+# print()
+# print(f"Runtime: {runtime:.4f} seconds")
